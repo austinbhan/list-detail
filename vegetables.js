9e9323896369ec573d6aa82e468ae46d6c28a566
austinbhan <austinbhan@outlook.com>
2022-05-09 17:31:34 -0700

    Array made

---
+++ vegetables.js
@@ -0,0 +1,6 @@
+export const vegetables = [
+    { id: 1, image: './assets/carrot.jpg', name: 'Carrot', color: 'Orange', price: '' },
+    { id: 2, image: '.assets/tomato.jpg', name: 'Tomato', color: 'Red', price: '' },
+    { id: 3, image: '.assets/beet.jpg', name: 'Beet', color: 'Purple', price: '' },
+    { id: 3, image: '.assets/potato.jpg', name: 'Potato', color: 'Brown', price: '' }    
+];
